2351dce7f1ad7106298cc2faaf6b7a6acb5efbd1
NikolaiBelic <57221840+NikolaiBelic@users.noreply.github.com>
2026-09-05 17:14:40 +0200

    Tune cinematic crystal bloom

--- apps/desktop/src/visualizers/crystal/effects/CrystalPostProcessing.tsx
+++ apps/desktop/src/visualizers/crystal/effects/CrystalPostProcessing.tsx
@@ -1,13 +1,18 @@
-import { Bloom, EffectComposer } from "@react-three/postprocessing";
+import { Bloom, EffectComposer, Vignette } from "@react-three/postprocessing";
 
 export function CrystalPostProcessing() {
   return (
     <EffectComposer multisampling={0}>
       <Bloom
-        intensity={1.35}
-        luminanceThreshold={0.85}
-        luminanceSmoothing={0.18}
+        intensity={1.75}
+        luminanceThreshold={1.05}
+        luminanceSmoothing={0.16}
         mipmapBlur
+      />
+      <Vignette
+        eskil={false}
+        offset={0.2}
+        darkness={0.72}
       />
     </EffectComposer>
   );
