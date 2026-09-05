25a329ca48ac9d79b3e1901c36142cf1f368286f
NikolaiBelic <57221840+NikolaiBelic@users.noreply.github.com>
2026-09-05 17:14:50 +0200

    Assemble cinematic crystal render pass

--- apps/desktop/src/visualizers/crystal/CrystalScene.tsx
+++ apps/desktop/src/visualizers/crystal/CrystalScene.tsx
@@ -1,8 +1,10 @@
 import { Canvas } from "@react-three/fiber";
+import { ACESFilmicToneMapping } from "three";
 
 import { CrystalAura } from "./CrystalAura";
 import { CrystalBody } from "./CrystalBody";
 import { CrystalInnerCore } from "./CrystalInnerCore";
+import { CrystalPostProcessing } from "./effects/CrystalPostProcessing";
 import { CrystalEnvironment } from "./lighting/CrystalEnvironment";
 import { CrystalLighting } from "./lighting/CrystalLighting";
 
@@ -10,31 +12,31 @@ interface CrystalSceneProps {
   accentColor: string;
 }
 
-export function CrystalScene({
-  accentColor,
-}: CrystalSceneProps) {
+export function CrystalScene({ accentColor }: CrystalSceneProps) {
   return (
     <Canvas
       camera={{
         position: [0, 0, 5],
-        fov: 42,
+        fov: 38,
       }}
       dpr={[1, 2]}
       gl={{
         antialias: true,
         alpha: true,
+        toneMapping: ACESFilmicToneMapping,
+        toneMappingExposure: 0.92,
       }}
     >
       <CrystalEnvironment accentColor={accentColor} />
-
-      <CrystalLighting accentColor={accentColor} />
-
+      <CrystalLighting />
       <CrystalAura accentColor={accentColor} />
 
-      <group scale={0.48}>
+      <group scale={0.72} position={[0, 0.08, 0]}>
         <CrystalInnerCore accentColor={accentColor} />
         <CrystalBody accentColor={accentColor} />
       </group>
+
+      <CrystalPostProcessing />
     </Canvas>
   );
 }
